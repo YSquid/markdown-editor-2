985b78432768d37448f0992607e467379d9311a3
YSquid <102336493+YSquid@users.noreply.github.com>
2022-09-01 10:45:46 -0600

    Install Gfm and setup default preview
    Installed GitHub Flavored Markdown (Gfm) and passed the remarkPlugins prop to the ReactMarkdown Element
    
    Setup the defaultPreview that loads using a Javascript variable set to a template literal (using backticks). Setup the required preview elements according to FCC project.
    
    Removed example card code I wasn't using

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, {useState, useEffect} from 'react'
 import ReactMarkdown from 'react-markdown'
+import remarkGfm from 'remark-gfm'
 import styled from 'styled-components'
 import 'bootstrap/dist/css/bootstrap.css';
 import './index.css'
@@ -8,11 +9,51 @@ import './index.css'
 
 function App() {
 
+  const defaultPreview = 
+`# H1
+## H2
+### H3
+
+Here is a link to [Google](https://google.com)
+
+Here is some \`inline code\`
+
+  this is a 
+    simple indented code block
+
+\`\`\`
+This is a fenced code block
+
+  const fruit = apple
+
+  if(fruit === apple) {
+    return true
+  }
+\`\`\`
+
+
+- list item
+
+> Block Quote
+
+
+**Here is some bolded text**
+
+Here is an image of a penguin! 
+
+![Penguin](https://cdn.download.ams.birds.cornell.edu/api/v1/asset/362635561/900)
+
+
+
+
+
+`
+
   const [inputValue, setInputValue] = useState('')
 
   useEffect (() => {
       setInputValue(
-        '# Hi, this is the *default text*'
+        defaultPreview
       )
 
   },[])
@@ -32,7 +73,7 @@ function App() {
 
       <div id='preview-card' className='card-body'>
         <h2 className='card-title'>Markdown Preview</h2>
-        <ReactMarkdown className='card-text'>{inputValue}</ReactMarkdown>
+        <ReactMarkdown remarkPlugins={[remarkGfm]} className='card-text'>{inputValue}</ReactMarkdown>
       </div>
 
 
--- src/card-example.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import 'bootstrap/dist/css/bootstrap.css';
-import ReactMarkdown from 'react-markdown'
-
-function cardExample() {
- 
-}
-
-export default cardExample;
